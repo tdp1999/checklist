5428d2b484f263e90215db9d5291a61608fe5150
tdp1999 <tdp99.business@gmail.com>
2022-02-28 13:43:29 +0700

    Finish Header components. Next task: Sidebar

--- src/app/layouts/blocks/header/header.component.ts
+++ src/app/layouts/blocks/header/header.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+    foods: any[] = [
+        { value: 'steak-0', viewValue: 'Steak' },
+        { value: 'pizza-1', viewValue: 'Pizza' },
+        { value: 'tacos-2', viewValue: 'Tacos' },
+    ];
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
--- src/app/layouts/blocks/header/header.module.ts
+++ src/app/layouts/blocks/header/header.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 
-// Material Components
+// Import routing module for
+import { RouterModule } from '@angular/router';
+
+// Import Material Components
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
-  imports: [CommonModule, MatToolbarModule, MatIconModule],
-  declarations: [HeaderComponent],
-  exports: [HeaderComponent],
+    imports: [
+        CommonModule,
+        RouterModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatChipsModule,
+        MatButtonModule,
+        MatSelectModule,
+    ],
+    declarations: [HeaderComponent],
+    exports: [HeaderComponent],
 })
 export class HeaderModule {}
