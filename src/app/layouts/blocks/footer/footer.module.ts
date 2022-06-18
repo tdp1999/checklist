04bcc18542714521aac850775f119a4fc8af9c8c
tdp1999 <tdp99.business@gmail.com>
2022-06-18 11:41:02 +0700

    update some style and styling footer

--- src/app/layouts/blocks/footer/footer.module.ts
+++ src/app/layouts/blocks/footer/footer.module.ts
@@ -2,17 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer.component';
 
-
+// Material modules
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [
-    FooterComponent
-  ],
-  exports: [
-    FooterComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+    declarations: [FooterComponent],
+    exports: [FooterComponent],
+    imports: [CommonModule, MatIconModule],
 })
-export class FooterModule { }
+export class FooterModule {}
